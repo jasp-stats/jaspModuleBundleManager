f74c0022b2587b049ab549a930ea70be799d62a6
RDOFFERHOFF <rdofferhoff@protonmail.com>
2024-07-17 14:20:36 +0200

    first commit

---
+++ R/installModule.R
@@ -0,0 +1,17 @@
+#' @export
+installJaspModuleBundle <- function(bundlePath, installPath) {
+
+  
+}
+
+#' @export
+removeJaspModuleBundle <- function(name, installPath) {
+
+  
+}
+
+#' @export
+createJaspModuleBundle <- function(modulePath, resultdir) {
+
+  
+}
